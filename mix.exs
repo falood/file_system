447b60c694689da32d14f698733ccaef67d63687
falood <falood@gmail.com>
2015-05-14 15:12:16 +0800

    Release 0.0.2.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExFSWatch.Mixfile do
 
   def project do
     [ app: :exfswatch,
-      version: "0.0.1-dev",
+      version: "0.0.2",
       elixir: "~> 1.0",
       deps: deps,
       description: "A file change watcher wrapper based on [fs](https://github.com/synrc/fs)",
