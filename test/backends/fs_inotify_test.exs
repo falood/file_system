d0c24adab6e3451809e7e84bc23a160d402f9e9a
falood <falood@gmail.com>
2017-07-22 19:33:31 -0700

    add whitespace in path test case

---
+++ test/backends/fs_inotify_test.exs
@@ -0,0 +1,48 @@
+defmodule FileSystem.Backends.FSInotifyTest do
+  use ExUnit.Case
+  import FileSystem.Backends.FSInotify
+
+  test "dir write close" do
+    assert {"/one/two/file", [:modified, :closed]} ==
+      ~w|/one/two/ CLOSE_WRITE,CLOSE file| |> to_port_line |> parse_line
+  end
+
+  test "dir create" do
+    assert {"/one/two/file", [:created]} ==
+      ~w|/one/two/ CREATE file| |> to_port_line |> parse_line
+  end
+
+  test "dir moved to" do
+    assert {"/one/two/file", [:renamed]} ==
+      ~w|/one/two/ MOVED_TO file| |> to_port_line |> parse_line
+  end
+
+  test "dir is_dir create" do
+    assert {"/one/two/dir", [:created, :isdir]} ==
+      ~w|/one/two/ CREATE,ISDIR dir| |> to_port_line |> parse_line
+  end
+
+  test "file write close" do
+    assert {"/one/two/file", [:modified, :closed]} ==
+      ~w|/one/two/file CLOSE_WRITE,CLOSE| |> to_port_line |> parse_line
+  end
+
+  test "file delete_self" do
+    assert {"/one/two/file", [:undefined]} ==
+      ~w|/one/two/file DELETE_SELF| |> to_port_line |> parse_line
+  end
+
+  test "whitespace in path" do
+    assert {"/one two/file", [:modified, :closed]} ==
+      ["/one two", "CLOSE_WRITE,CLOSE", "file"] |> to_port_line |> parse_line
+
+    assert {"/one/two/file 1", [:modified, :closed]} ==
+      ["/one/two", "CLOSE_WRITE,CLOSE", "file 1"] |> to_port_line |> parse_line
+
+    assert {"/one two/file 1", [:modified, :closed]} ==
+      ["/one two", "CLOSE_WRITE,CLOSE", "file 1"] |> to_port_line |> parse_line
+  end
+
+  defp to_port_line(list), do: list |> Enum.join(<<1>>) |> to_charlist
+
+end
