3b0849c31fc94c4779844e4b69dc801344151996
falood <falood@gmail.com>
2017-07-25 17:17:15 -0700

    update elixir version requirement

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FileSystem.Mixfile do
   def project do
     [ app: :file_system,
       version: "0.1.1",
-      elixir: "~> 1.3",
+      elixir: "~> 1.3 or ~> 1.4 or ~> 1.5",
       compilers: [:elixir, :app],
       deps: deps(),
       description: "A file system change watcher wrapper based on [fs](https://github.com/synrc/fs)",
